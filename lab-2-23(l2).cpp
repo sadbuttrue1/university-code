c030ce894a4abeadc955c2c02161a137ec06daee
Eugene Aslanov <aslanov.ev@gmail.com>
2012-03-05 22:18:57 +0400

    rc3

--- lab-2-23(l2).cpp
+++ lab-2-23(l2).cpp
@@ -14,7 +14,7 @@ void p_output(p_const *head)
 	p_const *p=head;
 	while(p)
 	{
-		if (p->count!=0.0) printf("%Ld) %f\n",p->num,p->count);
+		printf("%Ld) %f\n",p->num,p->count);
 		p=p->next;
 	}
 }
@@ -28,12 +28,15 @@ p_const *p_init(p_const *head)
 	printf("Введите коэффициенты (разделяя пробелом или переводом строки):\n");
 	for (long long int i=0; i<n; i++)
 	{
-		p_const *p=new p_const;
 		scanf("%f",&x);
-		p->count=x;
-		p->num=i;
-		p->next=head;
-		head=p;
+		if (x!=0)
+		{
+			p_const *p=new p_const;
+			p->count=x;
+			p->num=i;
+			p->next=head;
+			head=p;
+		}
 	}
 	return head;
 }
@@ -51,43 +54,48 @@ void p_delete(p_const *head)
 
 p_const *p_search_num(p_const *head, unsigned long long int num)
 {
-	p_const *p=head;
-	while (p && (p->num != num)) p=p->next;
+	p_const *p=head,*q,*t;
+	bool equal=false,insert=false;
+	while (p && (!equal) && (!insert))
+	{
+		q=p;
+		p=p->next;
+		if ((p!=NULL) && (p->num==num)) equal=true;
+		else if ((p!=NULL) && (q->num>num) && (p->num<num))
+				{
+					t=new p_const;
+					q->next=t;
+					t->num=num;
+					t->next=p;
+					insert=true;
+				}
+	}
+	if (insert)
+	{
+		p=t;
+	}
 	return p;
 }
 
 p_const *p_replace(p_const *head)
 {
 	p_output(head);
-	printf("1) Изменить существующий коэффициент\n");
-	printf("2) Добавить новый коэффициент\n");
-	char c;
-	scanf("\n%c",&c);
 	float count;
 	unsigned long long int num;
 	p_const *p;
-	switch (c)
+	printf("Введите номер коэффициента, который вы хотите изменить или создать:");
+	scanf("%Ld",&num);
+	printf("Введите новое значение коэффициента:");
+	scanf("%f",&count);
+	p=p_search_num(head,num);
+	if (p!=NULL) p->count=count;
+	else 
 	{
-		case '1':
-			{
-				printf("Введите номер коэффициента, который вы хотите изменить:");
-				scanf("%Ld",&num);
-				printf("Введите новое значение коэффициента:");
-				scanf("%f",&count);
-				p=p_search_num(head,num);
-				p->count=count;
-			}; break;
-		case '2':
-			{
-				printf("Введите значение коэффициента:");
-				scanf("%f",&count);
-				p=new p_const;
-				p->count=count;
-				num=head->num + 1;
-				p->num=num;
-				p->next=head;
-				head=p;
-			}; break;
+		p=new p_const;
+		p->num=num;
+		p->count=count;
+		p->next=head;
+		head=p;
 	}
 	printf("Новые коэффициенты:\n");
 	p_output(head);
@@ -175,7 +183,11 @@ void main_menu()
 			switch (choice)
 			{
 				case '0': printf("Написать программу, реализующую в виде списка представление многочлена P(x)=a_0+a_1*x+...+a_i*x^i, где а_i - вещественные числа, i - целые положительные числа, причем, если a_i=0, то соответствующий элемент-слагаемое должен отсутствовать в списке. Пользователь должен иметь возможность произвольно добавлять элементы-слагаемые через меню. Реализовать функцию сложения двух многочленов.\n");break;
-				case '1': {head1=p_init(head1);h1=true;}break;
+				case '1':
+				{
+					head1=p_init(head1);
+					h1=true;
+				}break;
 				case '2': d=false;break;
 			}
 		}
@@ -188,8 +200,18 @@ void main_menu()
 					case '0': printf("Написать программу, реализующую в виде списка представление многочлена P(x)=a_0+a_1*x+...+a_i*x^i, где а_i - вещественные числа, i - целые положительные числа, причем, если a_i=0, то соответствующий элемент-слагаемое должен отсутствовать в списке. Пользователь должен иметь возможность произвольно добавлять элементы-слагаемые через меню. Реализовать функцию сложения двух многочленов\n");break;
 					case '1': p_output(head1);break;
 					case '2': head1=p_replace(head1);break;
-					case '3': {printf("Введите значение X:");double s; s=p_calc(head1);printf("Значение P(x)=%f",s);}break;
-					case '4': {head2=p_init(head2);h2=true;}break;
+					case '3':
+					{
+						printf("Введите значение X:");
+						double s;
+						s=p_calc(head1);
+						printf("Значение P(x)=%f",s);
+					}break;
+					case '4':
+					{
+						head2=p_init(head2);
+						h2=true;
+					}break;
 					case '5': d=false;break;
 				}
 			}
@@ -200,10 +222,22 @@ void main_menu()
 					case '0': printf("Написать программу, реализующую в виде списка представление многочлена P(x)=a_0+a_1*x+...+a_i*x^i, где а_i - вещественные числа, i - целые положительные числа, причем, если a_i=0, то соответствующий элемент-слагаемое должен отсутствовать в списке. Пользователь должен иметь возможность произвольно добавлять элементы-слагаемые через меню. Реализовать функцию сложения двух многочленов.\n");break;
 					case '1': p_output(head1);break;
 					case '2': head1=p_replace(head1);break;
-					case '3': {printf("Введите значение X:");double s; s=p_calc(head1);printf("P(x)=%f\n",s);}break;
+					case '3':
+					{
+						printf("Введите значение X:");
+						double s;
+						s=p_calc(head1);
+						printf("P(x)=%f\n",s);
+					}break;
 					case '4': p_output(head2);break;
 					case '5': head2=p_replace(head2);break;
-					case '6': {printf("Введите значение Y:");double s; s=p_calc(head2);printf("G(y)=%f\n",s);}break;
+					case '6':
+					{
+						printf("Введите значение Y:");
+						double s;
+						s=p_calc(head2);
+						printf("G(y)=%f\n",s);
+					}break;
 					case '7': printf("P(x)+G(y)=%f\n",p_sum(head1,head2));break;
 					case '8': d=false;break;
 				}
